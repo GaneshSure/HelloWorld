71eab6aa5a55cb6f5a9d89a1f822f1b8f5e4349a
GaneshSure <32676546+GaneshSure@users.noreply.github.com>
2019-12-25 22:05:49 +0530

    Update Test.java
    adding for jenkins job test utomatically

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,8 @@ public class Test {
 	public static void main(String[] args) {
 		for (int i = 0; i < 10; i++) {
 			System.out.println("India : " + " Andhra Pradesh " + i);
+ // adding for jenkins job test utomatically
+
 				}
 	}
 }
