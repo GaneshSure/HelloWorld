97d97d5b30195e252f43b92421525c4bd1c7d512
GaneshSure <32676546+GaneshSure@users.noreply.github.com>
2019-12-25 22:44:48 +0530

    Update Test.java
    testing Jenkins job run automatically

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 		for (int i = 0; i < 10; i++) {
 			System.out.println("India : " + " Andhra Pradesh " + i);
  // adding for jenkins job test utomatically
+			//testing jenkins job run automatically 
 
 				}
 	}
